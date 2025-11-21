1b321536efc4e746fa5ae342f9acc4ae9d999e55
Rafael Lima <83765700+rsantosdlima@users.noreply.github.com>
2025-11-21 02:04:07 +0000

    ajustes para remover charset e locale dos tsconfig.json e adicionar exibição do salário bruto de 2025 no simulador de IRRF 2026.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  experimental: {
+    optimizeCss: true
+  }
 };
 
 module.exports = nextConfig;
