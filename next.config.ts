d35607d56fbb59d557d201518416aa90d5861726
rsantosdlima <rsantosdlima@gmail.com>
2025-11-23 21:54:50 -0300

    correção no next.config.ts para suportar imagens não otimizadas e trailing slash

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true, // Forces /about/index.html structure
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
